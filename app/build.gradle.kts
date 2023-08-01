63eeab83c043704dc179853d8d7a654de32370ed
Mike <mikeklintonlau@gmail.com>
2023-08-02 00:48:00 +0800

    Updated File

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,14 +8,7 @@ plugins {
 }
 
 android {
-    signingConfigs {
-        create("release") {
-            storeFile = file("keystore.jks")
-            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
-            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
-            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
-        }
-    }
+
     namespace = "com.mikelau.pokedex"
     compileSdk = 33
 
@@ -36,7 +29,6 @@ android {
             isDebuggable = false
             isMinifyEnabled = true
             isShrinkResources = true
-            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
