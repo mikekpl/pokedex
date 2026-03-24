e0a6f98b2897250000b8f955cdd296ab923d9dcf
Mike Lau <>
2026-03-24 23:57:09 +0800

    Added WindowCompat to address status bar color being white in system dark theme

--- app/src/main/java/com/mikelau/pokedex/MainActivity.kt
+++ app/src/main/java/com/mikelau/pokedex/MainActivity.kt
@@ -11,8 +11,13 @@ import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.mikelau.core.common.utils.ColorBackground
 import com.mikelau.pokedex.navigation.AppNavGraph
 import com.mikelau.pokedex.navigation.rememberAppNavBackStack
@@ -33,6 +38,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
+    val view = LocalView.current
+    SideEffect {
+        val window = (view.context as android.app.Activity).window
+        window.statusBarColor = Color.Transparent.toArgb()
+        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+    }
+
     Surface(
         modifier = Modifier
             .background(ColorBackground)
